adda82580bb80e5cedc1575cb7a0e08ba9852737
mtheuslipe <matheusfelipe.mf454@gmail.com>
2022-12-19 16:34:19 -0300

    esquema de repetição na calculadora

--- src/main/kotlin/calculator.kt
+++ src/main/kotlin/calculator.kt
@@ -2,26 +2,34 @@ import java.util.Scanner
 
 fun main(){
     println("Vamos à calculadora!")// iniciando a calculadora
+    // inicio do loop.
+    do {
+        println("Digite um número: ") //perguntando o primeiro núnmero
+        val input = Scanner(System.`in`) // lendo o input do console
+        val a = input.nextInt( ) // armazenando o input na primeira variável
+        println("Digite mais um número: ") //perguntando o segundo núnmero
+        val b = input.nextInt() // armazenando o input na segunda variável
 
-    println("Digite um número: ")//perguntando o primeiro núnmero
-    val input = Scanner(System.`in`)// lendo o input do console
-    val a = input.nextInt()// armazenando o input na primeira variável
-    println("Digite mais um número: ")//perguntando o segundo núnmero
-    val b= input.nextInt()// armazenando o input na segunda variável
+        println("1- Soma, 2- Subtração, 3- Multiplicação, 4- Divisão") // dando um norte sobre a escolha da operação
+        println("Para escolher a operação realizada, digite um número de 1 até 4:") // pedindo o número referente a operação
 
-    println("1- Soma, 2- Subtração, 3- Multiplicação, 4- Divisão")// dando um norte sobre a escolha da operação
-    println("Para escolher a operação realizada, digite um número de 1 até 4:")// pedindo o número referente a operação
-    val case = input.nextInt()// armazenando o digito da operação
+        //estrutura de caso que correlaciona o digito da operação com a operação específica
+        when (input.nextInt()) {
+            1 -> sum(a, b)
+            2 -> sub(a, b)
+            3 -> multi(a, b)
+            4 -> division(a, b)
+            else -> error("Digite apenas números de 1 até 4!!!")
+        }
 
-    //estrutura de caso que correlaciona o digito da operação com a operação específica
-    when(case){
-        1 -> sum(a, b)
-        2 -> sub(a,b)
-        3 -> multi(a,b)
-        4 -> division(a,b)
-    }
+        //criando estrutura para verificar se o usuário quer permanecer na calculadora ou finalizar o processo
+        println("1- Recomeçar, 2- Terminar") //legenda sobre o que precisa ser digitado
+        println("Segundo a legenda acima, digite um número para terminar ou começar de novo a calculadora: ")// comando ao usuário
+        if(input.nextInt() ==2) return //verificação para a função finalizar
+    }while (input.nextInt() == 1 ) // verificação do loop
+    println("Obrigado!")
 }
-
+// as funções especifícas da calculadora com as 4 operações básicas
 fun sum(number1: Int, number2: Int) {
     val sum = number1+number2
     println(sum)
@@ -33,11 +41,11 @@ fun sub(number1: Int, number2: Int){
 }
 
 fun multi(number1: Int, number2: Int){
-    val multi = number1 * number1
+    val multi = number1 * number2
     println(multi)
 }
 
-fun division(number1: Int, number2: Int){
-    val div = number1 / number2
+fun division(number1: Int, number2: Int) {
+    val div: Double = number1.toDouble() / number2.toDouble()
     println(div)
 }
