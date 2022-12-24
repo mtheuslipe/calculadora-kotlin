0d08460785abd097df0a7f2a231d66d246a8c297
mtheuslipe <matheusfelipe.mf454@gmail.com>
2022-12-24 12:24:48 -0300

    substituição de funções específicas para função única com object key word

--- src/main/kotlin/calculator.kt
+++ src/main/kotlin/calculator.kt
@@ -6,46 +6,38 @@ fun main(){
     do {
         println("Digite um número: ") //perguntando o primeiro núnmero
         val input = Scanner(System.`in`) // lendo o input do console
-        val a = input.nextInt( ) // armazenando o input na primeira variável
+        val number1 = input.nextInt() // armazenando o input na primeira variável
         println("Digite mais um número: ") //perguntando o segundo núnmero
-        val b = input.nextInt() // armazenando o input na segunda variável
+        val number2 = input.nextInt() // armazenando o input na segunda variável
 
         println("1- Soma, 2- Subtração, 3- Multiplicação, 4- Divisão") // dando um norte sobre a escolha da operação
         println("Para escolher a operação realizada, digite um número de 1 até 4:") // pedindo o número referente a operação
+        val operationCode= input.nextInt()//armazenando em uma variável para fazer a verificação no objeto
 
-        //estrutura de caso que correlaciona o digito da operação com a operação específica
-        when (input.nextInt()) {
-            1 -> sum(a, b)
-            2 -> sub(a, b)
-            3 -> multi(a, b)
-            4 -> division(a, b)
-            else -> error("Digite apenas números de 1 até 4!!!")
-        }
+        //chamando a função única com todas as operações em um único objeto
+        mathOperations(number1, number2, operationCode )
 
         //criando estrutura para verificar se o usuário quer permanecer na calculadora ou finalizar o processo
         println("1- Recomeçar, 2- Terminar") //legenda sobre o que precisa ser digitado
         println("Segundo a legenda acima, digite um número para terminar ou começar de novo a calculadora: ")// comando ao usuário
-        if(input.nextInt() ==2) return //verificação para a função finalizar
-    }while (input.nextInt() == 1 ) // verificação do loop
+        val verificationCode: Int = input.nextInt()//armazenando em variável se o usuário quer fazer outra conta ou encerrar
+        if(verificationCode ==2) return //verificação para a função finalizar
+    }while (verificationCode == 1 ) // verificação do loop
     println("Obrigado!")
 }
-// as funções especifícas da calculadora com as 4 operações básicas
-fun sum(number1: Int, number2: Int) {
-    val sum = number1+number2
-    println(sum)
-}
-
-fun sub(number1: Int, number2: Int){
-    val sub = number1 - number2
-    println(sub)
-}
-
-fun multi(number1: Int, number2: Int){
-    val multi = number1 * number2
-    println(multi)
-}
-
-fun division(number1: Int, number2: Int) {
-    val div: Double = number1.toDouble() / number2.toDouble()
-    println(div)
+//função única utilizando objeto deixando o código menos verboso e todas as instâncias e funções em apenas uma
+fun mathOperations(number1: Int, number2: Int, operationCode: Int){
+    //armazenando o objeto em uma variável
+    var operations = object {
+        //realizando a verificação de qual operação realizar de acordo com o que o usuário pediu
+         val result: Any = when(operationCode) {
+            1 -> number1 + number2
+            2 -> number1 - number2
+            3 -> number1 * number2
+            4 -> number1 / number2.toDouble()
+            else -> error("error 01: digite um número válido entre 1 e 4! (Operation Code: $operationCode)")
+        }
+    }
+    //mostrando o resultado
+    println("o resultado é: ${operations.result}")
 }
